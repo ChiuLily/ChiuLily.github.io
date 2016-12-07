6312ebf1740c718843106b752f124b1bd0e9a1ca
chiu <she963258@gmail.com>
2016-12-07 10:42:15 +0800

    test

--- js/index.js
+++ js/index.js
@@ -7,28 +7,27 @@ $(function() {
 });
 
 
-// // <!-- nav scroll -->
-// $(document).ready(function() {
-//     $('nav li a').click(function() {
-//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//             var $target = $(this.hash);
-//             $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
-//             if ($target.length) {
-//                 var targetOffset = $target.offset().top;
-//                 $('html,body')
-//                     .animate({
-//                         scrollTop: targetOffset
-//                     }, 1000);
-//                 return false;
-//             }
-//         }
-//     });
-
+// <!-- nav scroll -->
+$(document).ready(function() {
+    $('nav li a').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body')
+                    .animate({
+                        scrollTop: targetOffset
+                    }, 1000);
+                return false;
+            }
+        }
+    });
+// $('[data-spy="scroll"]').each(function() {
+//     var $spy = $(this).scrollspy('refresh')
 // });
 
-$('[data-spy="scroll"]').each(function () {
-  var $spy = $(this).scrollspy('refresh')
-});
+// });
 // $(function() {
 
 //     "use strict"; // Start of use strict
